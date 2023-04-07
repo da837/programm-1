bb2d793b87c9af4b940b2355c23a137cd7ed1e20
Daniel fantahun <129787744+da837@users.noreply.github.com>
2023-04-08 06:59:16 +0900

    Add files via upload

---
+++ home work 1 (2).cpp
@@ -0,0 +1,15 @@
+Code:
+if x > y:
+    print("x is greater than y")
+
+Output if x = 3 and y = 2:
+x is greater than y
+
+Output if x = 3 and y = 4:
+(No output)
+
+Output if x = 2 and y = 2:
+(No output)
+
+
+
